90605bacf60ab97aa581e567eeb4f323c135e628
Josh Paulin <3893760+paulinjo@users.noreply.github.com>
2026-08-08 10:09:15 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,22 @@
+import heapq
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min.append(val if not self.min else min(val, self.min[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
