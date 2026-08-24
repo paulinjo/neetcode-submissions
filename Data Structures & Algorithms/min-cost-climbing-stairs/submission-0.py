b10899efb4233a470ec363e30128d5b733a59ab4
Josh Paulin <3893760+paulinjo@users.noreply.github.com>
2026-08-24 13:18:52 -0400

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,19 @@
+import math
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cache = [None] * len(cost)
+
+        def dfs(i):
+            if i == len(cost):
+                return 0
+
+            if i > len(cost):
+                return math.inf
+
+            if cache[i]:
+                return cache[i]
+
+            cache[i] = cost[i] + min(dfs(i+1), dfs(i+2))
+            return cache[i]
+        return min(dfs(0), dfs(1))
