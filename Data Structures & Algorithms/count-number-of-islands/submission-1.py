25110ab9da120ecc820058f9dbb8c521c140f67f
Josh Paulin <3893760+paulinjo@users.noreply.github.com>
2026-09-02 14:38:24 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,30 @@
+ADJACENT = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        height, width = len(grid), len(grid[0])
+        results = 0
+        
+        def dfs(x: int, y: int):
+            if (x, y) in visited:
+                return
+
+            visited.add((x, y))
+
+            for dx, dy in ADJACENT:
+                nx, ny = x+dx, y+dy
+                if nx < 0 or nx >= width or ny < 0 or ny >= height:
+                    continue
+
+                if grid[ny][nx] == '0':
+                    continue
+
+                dfs(nx, ny)
+        
+        for y in range(height):
+            for x in range(width):
+                if grid[y][x] == "1" and (x, y) not in visited:
+                    results += 1
+                    dfs(x, y)
+        return results
